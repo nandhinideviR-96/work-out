c4a41762a0e09c5f783b435be34b9ab8f5349239
Nandhini Devi R <nandhinideviofficial@gmail.com>
2025-02-20 20:33:33 +0530

    Add files via upload

---
+++ ZeroOneTriangle.java
@@ -0,0 +1,30 @@
+public class ZeroOneTriangle {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        //outer loop to handle number of rows
+        for (int i = 1; i <= n; i++) {
+            //inner loop to handle number of columns
+            for (int j = 1; j <= i; j++) {
+                // if the sum of (i+j) is even then print 1
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                }
+                // otherwise print 0
+                else {
+                    System.out.print(0 + " ");
+                }
+            }
+
+            //printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
